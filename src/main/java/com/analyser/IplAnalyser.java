05e61d62df63b39dbb5329d4bca8681814ce0364
shrawanamit <amitkumar06111@gmail.com>
2020-05-19 22:20:45 +0530

    added test and code to sort on batting average and return into json data

--- src/main/java/com/analyser/IplAnalyser.java
+++ src/main/java/com/analyser/IplAnalyser.java
@@ -1,22 +1,22 @@
 package com.analyser;
+
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-
-
+import java.util.stream.Collectors;
 
 public class IplAnalyser {
 
-
     List<IplRunsDAO> iplRunsList;
-
     public IplAnalyser() {
         this.iplRunsList = new ArrayList<>();
     }
 
     public int loadIplMostRunData(String csvFilePath) throws IplAnalyserException{
+
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
             Iterator<IplMostRunsCSV> csvFileIterator = csvBuilder.getCSVFileIterator(reader, IplMostRunsCSV.class);
@@ -27,5 +27,23 @@ public class IplAnalyser {
         } catch (IOException | CSVBuilderException e) {
             throw new IplAnalyserException(e.getMessage(), IplAnalyserException.ExceptionType.IPL_FILE_PROBLEM);
         }
+    }
+
+    public String loadSortedOnBattingAverage() throws IplAnalyserException {
+
+        Comparator<IplRunsDAO> averageComparator =Comparator.comparing(census -> census.average);
+        return sort(averageComparator);
+    }
+
+    private String sort(Comparator<IplRunsDAO> averageComparator) throws IplAnalyserException {
+
+        if(iplRunsList == null || iplRunsList.size() ==0 ) {
+            throw new IplAnalyserException("no runs data",IplAnalyserException.ExceptionType.NO_IPL_DATA);
+        }List sortedAvgRunsData =iplRunsList
+                                    .stream()
+                                    .sorted(averageComparator)
+                                    .collect(Collectors.toList());
+            String sortedAverageRunsDataInJson = new Gson().toJson(sortedAvgRunsData);
+            return sortedAverageRunsDataInJson;
     }
 }
--- src/test/java/com/analyser/IplTest.java
+++ src/test/java/com/analyser/IplTest.java
@@ -1,19 +1,32 @@
 package com.analyser;
-
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+
 
 public class IplTest {
     private static final String IPL_2019_MOST_RUN_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL_2019_MOST_Wkts_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IPL2019FactsheetMostRuns.csv";
     @Test
-    public void givenIplMostRun_whenSortedOnBatingAverage_shouldReturnShortedResult() throws IplAnalyserException {
+    public void givenIplMostRunCSV_whenSortedOnBatingAverage_shouldReturnShortedResult() throws IplAnalyserException {
         IplAnalyser iplAnalyser = new IplAnalyser();
         int noOfRecord = iplAnalyser.loadIplMostRunData(IPL_2019_MOST_RUN_CSV_FILE_PATH);
         Assert.assertEquals(101, noOfRecord);
 
+    }
+    @Test
+    public void givenIplMostRunCSV_whenSortedOnBattingAverage_shouldReturnShortedResult() {
+
+        try {
+            IplAnalyser iplAnalyser = new IplAnalyser();
+            iplAnalyser.loadIplMostRunData(IPL_2019_MOST_RUN_CSV_FILE_PATH);
+            String sortedResult = iplAnalyser.loadSortedOnBattingAverage();
+            IplMostRunsCSV[] IplDataCSV = new Gson().fromJson( sortedResult , IplMostRunsCSV[].class);
+            Assert.assertEquals(0.0, IplDataCSV[0].average,0.001);
+        } catch ( IplAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 
 }
